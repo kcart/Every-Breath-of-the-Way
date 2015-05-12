b159e6844547fbd5cc5ca129ead4586428c1a32e
Kathleen Eve <kathleeneveKat@Kats-MacBook-Pro.local>
2015-05-11 22:03:04 -0700

    leaving for the day. I created the home page, and added a kitten to test text, and image rendering. TO DO: must figure out the issue with the registration form not showing up when connected to the local host. Good feeling for the end of the day(=^.^=)

--- model.py
+++ model.py
@@ -2,3 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+################################################################################################
+ 
+#User Definitions
+
+Class User(dn)
