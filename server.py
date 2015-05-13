1fedaa5f128760cd9475736a769d524e9a4c805c
Kathleen Eve <kathleeneveKat@Kats-MacBook-Pro.local>
2015-05-13 15:48:07 -0700

    committing changes to server.py

--- server.py
+++ server.py
@@ -3,12 +3,21 @@
 from jinja2 import StrictUndefined
 
 from flask import Flask, render_template, request, flash, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import connect_to_db, db, User, Attack, Attack_Symptom, Symptom, Attack_Trigger, Possible_Trigger, Trigger_Type
 
 app = Flask(__name__)
 
 app.jinja_env.undefined = StrictUndefined
 
- # Running into  template rendering issue for the registration form 
+
+@app.route('/')
+def index():
+	"""Site Homepage."""
+
+	return render_template("homepage.html")
+
 @app.route('/register', methods=['GET'])
 def register_form():
 	"""Show form for profile creation."""
@@ -20,12 +29,12 @@ def register_process():
 	"""Process Registration"""
 
 	email = request.form["email"]
-	fname = request.form["first"]
-	lname = request.form["last"]
+	first_name = request.form["first"]
+	last_name = request.form["last"]
 	password = request.form["password"]
 	age = int(request.form["age"])
 	
-	new_user = User(email=email, fname=fname, lname=lname, password=password, age=age)
+	new_user = User(email=email, first_name=fname, last_name=lname, password=password, age=age)
 
 	db.session.add(new_user)
 	db.session.commit()
@@ -33,12 +42,6 @@ def register_process():
 	flash("User Profile %s added." % email)
 	return redirect("/")
 
- #alphabetized routes for good practice 
-@app.route('/')
-def index():
-	"""Site Homepage."""
-
-	return render_template("homepage.html")
 
 
 
@@ -54,4 +57,15 @@ def index():
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.debug = True
+
+	connect_to_db(app)
+
+	DebugToolbarExtension(app)
+
+	app.run()
+
+
+
+
+
