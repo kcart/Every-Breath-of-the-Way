5ef0889bfca1c44b0d8dd4fdeeab35639a087f3f
Kathleen Eve <kathleeneveKat@Kats-MacBook-Pro.local>
2015-05-15 18:31:44 -0700

    finished for the day

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db, User, Attack, Attack_Symptom, Symptom, Attack_Trigger, Possible_Trigger, Trigger_Type
+from model import connect_to_db, db, User, Attack, AttackSymptom, Symptom, AttackTrigger, PossibleTrigger, TriggerType
 
 app = Flask(__name__)
 
