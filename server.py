bdf2f5ed4321f689d5f74d193194b34991d3a1b0
kcart <llid09@gmail.com>
2015-08-27 23:10:51 -0400

    adding git ignore

--- server.py
+++ server.py
@@ -7,27 +7,12 @@ from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, User, Attack, AttackSymptom, AttackTrigger
 from collections import Counter
 from Crypto.Hash import SHA256
-import os
-import psycopg2
-import urlparse
 
 app = Flask(__name__)
 
 app.jinja_env.undefined = StrictUndefined
 
 app.secret_key = "wheeze89798ew7hjyas98798798sdhui7987s987bhghjg987r"
-
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
-
 
 @app.route('/')
 def index():
