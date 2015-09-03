6329b271eedd2c68a47936bee8974425320cc04e
kcart <llid09@gmail.com>
2015-09-02 21:53:36 -0400

    heroku

--- server.py
+++ server.py
@@ -25,8 +25,8 @@ conn = psycopg2.connect(
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/breathepostgres'
-heroku = Heroku(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/breathepostgres'
+# heroku = Heroku(app)
 db = SQLAlchemy(app)
 
 app.jinja_env.undefined = StrictUndefined
@@ -374,6 +374,6 @@ def logout():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     # connect_to_db(app)
     app.run()
